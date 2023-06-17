2bdda82085f56cce139db8feebc87abe6bca41f2
geff-mutua <geoffreymutua01@gmail.com>
2023-06-17 13:34:13 +0300

    Defined the login in the main service Class

--- README.md
+++ README.md
@@ -45,29 +45,12 @@ $ npm run start:dev
 $ npm run start:prod
 ```
 
-## Test
-
-```bash
-# unit tests
-$ npm run test
-
-# e2e tests
-$ npm run test:e2e
-
-# test coverage
-$ npm run test:cov
-```
-
-## Support
-
-Nest is an MIT-licensed open source project. It can grow thanks to the sponsors and support by the amazing backers. If you'd like to join them, please [read more here](https://docs.nestjs.com/support).
-
 ## Stay in touch
 
-- Author - [Kamil Myśliwiec](https://kamilmysliwiec.com)
-- Website - [https://nestjs.com](https://nestjs.com/)
-- Twitter - [@nestframework](https://twitter.com/nestframework)
+- Developer - [Geoffrey mutua](https://geocadict.com)
+- Website - [https://geocadict.com](https://geocadict.com/)
+- Contact - [+254795451228](https://twitter.com/nestframework)
 
-## License
+## About the App
 
-Nest is [MIT licensed](LICENSE).
+This is simple api based project for showing how you can get started with Nest Js framework to create awesome backend.
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,60 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { ReportType, data } from "./data";
+import {v4 as uuid} from "uuid"
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+export class AppService{
+
+  getAllReports(type:ReportType){
+    return data.report.filter((report)=>report.type===type);
   }
+
+  getReportById(id:string,type:ReportType){
+    return data.report.filter((report)=>report.type===type).find((report)=>report.id===id);
+  }
+
+  createReport(body:{source:string,amount:number},type:ReportType){
+    const report={
+      id:uuid(),
+      source:body.source,
+      amount:body.amount,
+      created_at:new Date(),
+      updated_at:new Date(),
+      type:type
+    }
+    data.report.push(report);
+    return report;
+  }
+
+  updateReport(id:string,body:{source:string,amount:number},type:string){
+
+    // Find the report with the Id and the type
+    const reportToUpdate= data.report.filter((report)=>report.type===type).find((report)=>report.id===id);
+
+    if(!reportToUpdate) return;
+
+    // find the report index
+    const reportIndex=data.report.findIndex((report)=>report.id===reportToUpdate.id);
+
+    data.report[reportIndex]={...data.report[reportIndex],...body,updated_at:new Date()};
+
+    return data.report[reportIndex];
+
+  }
+
+  deleteReport(id:string,type:ReportType){
+    //ind the data to be deleted
+    const reportToDelete= data.report.filter((report)=>report.type===type).find((report)=>report.id===id);
+
+    //  if the report not found
+    if(!reportToDelete) return "No report found with that Id";
+
+     // find the report index
+    const reportIndex=data.report.findIndex((report)=>report.id===reportToDelete.id);
+
+    // Delete the report from the data
+    data.report.splice(reportIndex,1);
+
+  }
+
 }
